a7f6f2b095f89f8d567e63cd49f21a2d23d0bbe0
DaNNy <skaldus96@gmail.com>
2019-03-27 17:46:32 +0100

    Implement attack method;

--- app.js
+++ app.js
@@ -10,6 +10,37 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            var maxDamage = 10;
+            var minDamage = 3;
+            var damage = Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+            
+            maxDamage = 12;
+            minDamage = 5;
+            var damage = Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+            this.playerHealth -= damage;
+
+            if(this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
         }
     }
 });
