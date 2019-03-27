dd09669a34f2732a0e526f397b7ae1ee66241258
DaNNy <skaldus96@gmail.com>
2019-03-27 17:53:35 +0100

    Refactor attack method;

--- app.js
+++ app.js
@@ -6,41 +6,50 @@ new Vue({
         gameIsRunning: false
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function() {
-            var maxDamage = 10;
-            var minDamage = 3;
-            var damage = Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
-            this.monsterHealth -= damage;
+        attack: function () {
+            this.monsterHealth -= this.calculateDamage(3, 10);
 
-            if(this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
+            if(this.checkWin())
                 return;
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+
+            this.checkWin();
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        },
+        calculateDamage: function (minDamage, maxDamage) {
+            return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
             }
-            
-            maxDamage = 12;
-            minDamage = 5;
-            var damage = Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
-            this.playerHealth -= damage;
-
-            if(this.playerHealth <= 0) {
-                alert('You lost!');
-                this.gameIsRunning = false;
-            }
-        },
-        specialAttack: function() {
-
-        },
-        heal: function() {
-
-        },
-        giveUp: function() {
-
+            return false;
         }
     }
 });
