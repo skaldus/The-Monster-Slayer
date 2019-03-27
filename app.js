0b1ff68c02af5bc63ee8e55bdfd1a76af4dcb30b
DaNNy <skaldus96@gmail.com>
2019-03-27 18:10:16 +0100

    Implement action log;

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for ' + damage
+            });
 
             if (this.checkWin())
                 return;
@@ -20,7 +27,12 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function () {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster hard for ' + damage
+            });
 
             if (this.checkWin())
                 return;
@@ -33,15 +45,27 @@ new Vue({
             else
                 this.playerHealth = 100;
 
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player heals for 10'
+                });
+
             this.monsterAttacks();
         },
         giveUp: function () {
             this.gameIsRunning = false;
         },
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
 
             this.checkWin();
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for ' + damage
+            });
+
         },
         calculateDamage: function (minDamage, maxDamage) {
             return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
